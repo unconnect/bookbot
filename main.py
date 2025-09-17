1b02ea5094a3c96e05f7124291a4f7db644a5c64
Alexander Nikolas Reuber <info@unconnect.net>
2025-09-17 23:56:58 +0200

    Add words and char report

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-from stats import get_word_count
+from stats import get_word_count, get_character_count, get_sorted_dict
 
 def get_book_text(filepath):
   with open(filepath) as f:
     return f.read()
 
 def main():
-  book_text = get_book_text('./books/frankenstein.txt')
-  num_words = get_word_count(book_text)
-  print(f"{num_words} words found in the document")
+  book = 'books/frankenstein.txt'
+  book_text = get_book_text(book)
+  word_count = get_word_count(book_text)
+  char_counts = get_character_count(book_text)
+  sorted_alpha_counts = get_sorted_dict(char_counts)
+
+  # Create report
+  print("============ BOOKBOT ============")
+  print(f"Analyzing book found at {book}...")
+  print("----------- Word Count ----------")
+  print(f"Found {word_count} total words")
+  print("--------- Character Count -------")
+  for item in sorted_alpha_counts:
+    char = item["char"]
+    num = item["num"]
+    print(f"{char}: {num}")
+  print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,24 @@
 def get_word_count(text: str):
   words = text.split()
   return len(words)
+
+def get_character_count(text: str):
+  character_count = {}
+  for character in text:
+    current_char = character.lower()
+    if(current_char in character_count):
+      character_count[current_char] += 1
+    else:
+      character_count[current_char] = 1
+  return character_count
+
+def sort_on(items):
+  return items["num"]
+
+def get_sorted_dict(unsorted_dict: dict):
+  sorted_dicts = []
+  for (char, count) in unsorted_dict.items():
+    if(char.isalpha()):
+      sorted_dicts.append({"char": char, "num": count})
+  sorted_dicts.sort(reverse=True, key=sort_on)
+  return sorted_dicts
